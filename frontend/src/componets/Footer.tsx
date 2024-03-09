d03b7d216c2f83096c93e5b33dd679572ccbb2a2
ManikandanRJSM <rmanikandanrjsm@outlook.com>
2024-03-09 14:36:09 +0530

    nav and footer

--- frontend/src/componets/Footer.tsx
+++ frontend/src/componets/Footer.tsx
@@ -26,9 +26,10 @@ function Footer() {
                           </ul> */}
                       </div>
                   </div>
-                  <div className="col-lg-3">
-                      <p>
-                        Copyright &copy; {new Date().getFullYear()} All rights reserved | ManikandanRJSM <i className="fa fa-heart" aria-hidden="true"></i>
+                  
+                  <div className="col-lg-9">
+                        <p style={{marginLeft: "387px"}}>
+                            V1.0 Copyright &copy; {new Date().getFullYear()} All rights reserved | ManikandanRJSM <i className="fa fa-heart" aria-hidden="true"></i>
                         </p>
 
                     </div>
